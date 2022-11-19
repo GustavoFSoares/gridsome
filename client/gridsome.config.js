6f0c034af7b1f151541982a0a3104bcb5edb1992
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-11-19 12:40:33 -0300

    ✨ feat: added request to API

--- client/gridsome.config.js
+++ client/gridsome.config.js
@@ -5,6 +5,23 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome",
+  plugins: [
+    {
+      use: "gridsome-source-rest",
+      options: {
+        debug: false,
+        axiosConfig: undefined,
+        endpoint: "http://localhost:3000",
+        typeName: "Pokemons",
+        isStatic: false,
+        isCollection: true,
+        responseInterceptor: (responseData) => {
+          console.log(responseData);
+
+          return responseData
+        },
+      },
+    },
+  ],
+};
